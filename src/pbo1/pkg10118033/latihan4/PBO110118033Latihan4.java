4e619e2b196a9456f292e45a6f9e9bbbb6ef386c
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 22:57:53 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan4/PBO110118033Latihan4.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118033.latihan4;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan4 {
+
+    public void tambahKambing() {
+        int jmlhKambing = 0;
+        
+        jmlhKambing = jmlhKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah : " +jmlhKambing);
+    }
+    public static void main(String[] args) {
+        PBO110118033Latihan4 kambingJantan = new PBO110118033Latihan4();
+        kambingJantan.tambahKambing();
+    }
+}
